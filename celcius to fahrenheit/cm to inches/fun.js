cc3b914d54fd81090be977d370277d420d6c3dc5
Monikaboopathi <136053222+Monikaboopathi@users.noreply.github.com>
2023-08-30 20:07:44 +0530

    Add files via upload

---
+++ celcius to fahrenheit/cm to inches/fun.js
@@ -0,0 +1,10 @@
+function convert(){
+    let cm=Number(document.getElementById("input").value)
+    let inches=cm/2.54;
+    let result=document.getElementById("result")
+    result.innerHTML=inches.toFixed(3)+" "+"inches";
+    }
+    
+    
+    // finished project ans correct executed
+    
